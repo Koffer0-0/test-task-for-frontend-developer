8fef0c6cf270a5fd93eabe894a0a3e2dc3f47a87
Temka Akish <68071028+Koffer0-0@users.noreply.github.com>
2025-08-01 11:28:40 +0500

    feat: define cart functionality in store

--- src/stores/app.js
+++ src/stores/app.js
@@ -1,8 +1,32 @@
 // Utilities
-import { defineStore } from 'pinia'
+import {defineStore} from 'pinia'
 
 export const useAppStore = defineStore('app', {
 	state: () => ({
-		//
+		rawItems: [],
 	}),
+	getters: {
+		items: (state) =>
+			state.rawItems.reduce((items, item) => {
+				const existingItem = items.find((it) => it.name === item)
+
+				if (!existingItem) {
+					items.push({name: item, amount: 1})
+				} else {
+					existingItem.amount++
+				}
+
+				return items
+			}, []),
+	},
+	actions: {
+		addItem(item) {
+			this.rawItems.push(item)
+		},
+
+		removeItem(item) {
+			const i = this.rawItems.lastIndexOf(item)
+			if (i > -1) this.rawItems.splice(i, 1)
+		},
+	},
 })
